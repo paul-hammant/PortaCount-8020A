eacc1ea05ccbbdfbcfbdcad816125519ce868d76
Paul Hammant <paul@hammant.org>
2022-12-03 18:37:10 +0000

    Update portacount.py

--- portacount.py
+++ portacount.py
@@ -9,6 +9,8 @@ mmask_tot = 0
 aambient_tot = 0
 high_pct = 0
 low_pct = 99.9999999
+high_ff = 99999999
+low_ff = 0
 first = ""
 start = datetime.now()
 while True:
@@ -21,22 +23,24 @@ while True:
         aambient = float(words[1])
     if mmask > 0 and aambient > 0:
         pct = ((aambient - mmask) / aambient) * 100
+        ff = '%s' % float('%.3g' % (aambient / mmask))
         if pct > high_pct:
             high_pct = pct
+            high_ff = ff
         if pct < low_pct:
             low_pct = pct
+            low_ff = ff
         mmask_tot = mmask_tot + mmask
         aambient_tot = aambient_tot + aambient
-        pct_ave = ((aambient_tot - mmask_tot) / aambient_tot) * 100
         run = run + 1
-        sys.stdout.write("Run " + str(run) + ": " + str("%.4f" % pct) + "%, ave so far: " + str("%.4f" % pct_ave) + "%\n")
+        sys.stdout.write("Ex " + str(run) + ": " + str("%.4f" % pct) + "%, Fit-Factor " + ff + "\n")
         mmask = 0
         aambient = 0
     if words[0] == "Overall":
         pct_ave = ((aambient_tot - mmask_tot) / aambient_tot) * 100
-        sys.stdout.write("Overall Average: " + str("%.4f" % pct_ave) + "%\n")
-        sys.stdout.write("High: " + str("%.4f" % high_pct) + "%\n")
-        sys.stdout.write("Low: " + str("%.4f" % low_pct) + "%\n")
+        sys.stdout.write("Average: " + str("%.4f" % pct_ave) + "%, Fit-Factor " + str('%s' % float('%.3g' % (aambient_tot / mmask_tot))) + "\n")
+        sys.stdout.write("High: " + str("%.4f" % high_pct) + "%, Fit-Factor: " + high_ff + "\n")
+        sys.stdout.write("Low: " + str("%.4f" % low_pct) + "%, Fit-Factor: " + low_ff + "\n")
         sys.stdout.write("Duration: {:.2f} mins\n".format((datetime.now() - start).total_seconds() / 60))
         exit(0)
     if words[0] == "Ave." and words[1] == "Conc.":
